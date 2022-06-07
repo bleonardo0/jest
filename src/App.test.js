179ba9c7216dae4e1d2c2b8362100fdf2299c78d
Leonardo <leonardo.basbous@chalhoub.com>
2022-06-07 11:19:41 +0400

    nice

---
+++ src/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+// test('button have correct color', () => {
+//   render(<App />);
+//   const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+//   expect(colorButton).toHaveStyle({'background-color': 'red'});
+// });
+
+test('button has correct initial text', () => {
+});
+
+test('button turn blue when click', () => {
+});
